f0199175953b4f16bdb70ea7b16bae8c1482b68f
Joshua Sommers <Joshuadsommers@gmail.com>
2016-10-08 20:17:40 -0700

    Initial activity

---
+++ app/src/main/java/com/example/josh/anagramapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.josh.anagramapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+//Main activity
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
